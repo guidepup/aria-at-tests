8b8af676d1c1d97150861bed9755de6081b96f4c
Craig Morten <cmorten@users.noreply.github.com>
2022-09-19 16:53:29 +0100

    test: add breadcrumb tests (#3)

--- tests/assert.ts
+++ tests/assert.ts
@@ -1,18 +1,24 @@
 import { expect } from "@playwright/test";
 
-export function assert({ voiceOver, phrase, range = 2 }) {
-  const acceptableSpokenPhraseLog = voiceOver
-    .spokenPhraseLog()
-    .slice(-1 * range);
+export function assert({ voiceOver, phrase }) {
+  const spokenPhraseLog = voiceOver.spokenPhraseLog();
+  const startIndex = spokenPhraseLog.length - 1 - [...spokenPhraseLog]
+    .reverse()
+    .findIndex((spokenPhrase) => spokenPhrase.includes("Run Test Setup"));
+  const testSpokenPhraseLog = spokenPhraseLog.slice(startIndex);
 
-  const found = !!acceptableSpokenPhraseLog.find((spokenPhrase) =>
+  const found = !!testSpokenPhraseLog.find((spokenPhrase) =>
     spokenPhrase.includes(phrase)
   );
 
   if (!found) {
     console.error(
       `Unable to find phrase "${phrase}" in spoken phrase log:\n\n${JSON.stringify(
-        acceptableSpokenPhraseLog,
+        testSpokenPhraseLog,
+        undefined,
+        2
+      )}\n\nFull spoken phrase log:\n\n${JSON.stringify(
+        spokenPhraseLog,
         undefined,
         2
       )}`
